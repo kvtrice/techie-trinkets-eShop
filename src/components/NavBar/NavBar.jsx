611b4da25b248d2ad181c7a0c7f41987296e8108
kvtrice <katricemountford@gmail.com>
2024-07-27 15:56:48 +1000

    Adds variables and mixins

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,27 @@
 import styles from "./NavBar.module.scss";
 import { NavLink } from "react-router-dom";
+import { BsCartFill } from "react-icons/bs";
 
 const NavBar = () => {
 	return (
 		<div className={styles.nav}>
 			<div></div>
-			<NavLink to="/" className={styles.nav__title}>Techie Trinketts</NavLink>
-			<NavLink to="/cart" className={styles.nav__cart}>Cart</NavLink>
+			<NavLink
+				to="/"
+				className={styles.nav__title}
+			>
+				Techie Trinketts
+			</NavLink>
+			<NavLink
+				to="/cart"
+				className={styles.nav__cart}
+			>
+				<BsCartFill
+					className={styles.nav__cart__icon}
+					size={25}
+					fill="#000000"
+				/>
+			</NavLink>
 		</div>
 	);
 };
