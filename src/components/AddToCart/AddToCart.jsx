a68d68cc44cb236332901899824ffd56c9146b85
kvtrice <katricemountford@gmail.com>
2024-07-28 15:13:32 +1000

    Adds select to change variant style

---
+++ src/components/AddToCart/AddToCart.jsx
@@ -0,0 +1,6 @@
+import styles from "./AddToCart.module.scss";
+
+const AddToCart = () => {
+	return <div>AddToCart</div>;
+};
+export default AddToCart;
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import AddToCart from "../AddToCart/AddToCart";
 import styles from "./SingleProduct.module.scss";
 
 const SingleProduct = ({ product }) => {
+	const [currentImage, setCurrentImage] = useState(
+		product?.variants[0]?.image
+	);
+	const [currentStyle, setCurrentStyle] = useState();
+
+	const handleStyleChange = style => {
+		setCurrentStyle(style);
+		const variant = product.variants.find(
+			variant => variant.style === style
+		);
+		if (variant) {
+			setCurrentImage(variant.image);
+		}
+	};
+
 	return (
 		<div className={styles.product}>
 			<div className={styles.product__info}>
@@ -10,7 +27,7 @@ const SingleProduct = ({ product }) => {
 					>
 						<img
 							className={styles.product__info__images__main}
-							src={product.variants[0].image}
+							src={currentImage}
 						/>
 					</div>
 					<div
@@ -24,6 +41,7 @@ const SingleProduct = ({ product }) => {
 									styles.product__info__images__variant
 								}
 								key={variant.style}
+								onClick={() => handleStyleChange(variant.style)}
 							>
 								<img
 									className={
@@ -42,7 +60,32 @@ const SingleProduct = ({ product }) => {
 					<p className={styles.product__info__content__description}>
 						{product.description}
 					</p>
-					<p>{product.price}</p>
+					<p className={styles.product__info__content__price}>
+						${product.price}
+					</p>
+					<div
+						className={
+							styles.product__info__content__styleContainer
+						}
+					>
+						<select
+							name="style"
+							id="style"
+							className={styles.product__info__content__style}
+							onChange={e => handleStyleChange(e.target.value)}
+							value={currentStyle}
+						>
+							{product.variants?.map(variant => (
+								<option
+									key={variant.style}
+									value={variant.style}
+								>
+									{variant.style}
+								</option>
+							))}
+						</select>
+					</div>
+					<AddToCart />
 				</div>
 			</div>
 		</div>
